22c9c005d03264a800b3f992c0f87318e33273ba
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-08-23 21:22:02 -0400

    Refactor index.js to accomodate testing

--- services/metadata/src/index.js
+++ services/metadata/src/index.js
@@ -2,40 +2,20 @@ const express = require("express");
 const mongodb = require("mongodb");
 const bodyParser = require("body-parser");
 
-if (!process.env.DBHOST) {
-    throw new Error("Please specify the databse host using environment variable DBHOST.");
-}
-
-if (!process.env.DBNAME) {
-    throw new Error("Please specify the name of the database using environment variable DBNAME");
-}
-
-const DBHOST = process.env.DBHOST;
-const DBNAME = process.env.DBNAME;
-
-//
-// Connect to the database.
-//
-function connectDb() {
-    return mongodb.MongoClient.connect(DBHOST) 
+function connectDb(dbhost, dbname) {
+    return mongodb.MongoClient.connect(dbhost) 
         .then(client => {
-            return client.db(DBNAME);
+            return client.db(dbname);
         });
 }
 
-//
-// Setup event handlers.
-//
 function setupHandlers(app, db) {
 
     const videosCollection = db.collection("videos");
 
-    //
-    // HTTP GET API to retrieve list of videos from the database.
-    //
     app.get("/videos", (req, res) => {
-        videosCollection.find() // Retreive video list from database.
-            .toArray() // In a real application this should be paginated.
+        videosCollection.find()
+            .toArray() // TODO Pagination
             .then(videos => {
                 res.json({ videos });
             })
@@ -47,35 +27,53 @@ function setupHandlers(app, db) {
     });
 }
 
-//
-// Start the HTTP server.
-//
 function startHttpServer(db,) {
-    return new Promise(resolve => { // Wrap in a promise so we can be notified when the server has started.
+    return new Promise(resolve => {
         const app = express();
-        app.use(bodyParser.json()); // Enable JSON body for HTTP requests.
+        app.use(bodyParser.json());
         setupHandlers(app, db);
 
         const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
         app.listen(port, () => {
-            resolve(); // HTTP server is listening, resolve the promise.
+            resolve();
         });
     });
 }
 
-//
-// Application entry point.
-//
+function startMicroservice(dbhost, dbname) {
+    return connectDb(dbhost, dbname)
+        .then(dbConn => {
+            return startHttpServer(dbConn);
+        });
+}
+
 function main() {
-    return connectDb()                      // Connect to the database...
-        .then(db => {                       // then...
-            return startHttpServer(db);     // start the HTTP server.
-        });
+    if (!process.env.DBHOST) {
+        throw new Error("Please specify the database host using environment variable DBHOST.");
+    }
+    
+    const DBHOST = process.env.DBHOST;
+
+    if (!process.env.DBNAME) {
+        throw new Error("Please specify the database name using environment variable DBNAME.");
+    }
+    
+    const DBNAME = process.env.DBNAME;
+        
+    return startMicroservice(DBHOST, DBNAME);
 }
 
-main()
-    .then(() => console.log("Microservice online."))
-    .catch(err => {
-        console.error("Microservice failed to start.");
-        console.error(err && err.stack || err);
-    });
+if (require.main === module) {
+    main()
+        .then(() => console.log("Microservice online."))
+        .catch(err => {
+            console.error("Microservice failed to start.");
+            console.error(err && err.stack || err);
+        });
+}
+else {
+    // Otherwise we are running under test
+    module.exports = {
+        startMicroservice,
+    };
+}
