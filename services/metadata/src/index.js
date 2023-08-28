9c8615e7c266651a15dfe689a2c0340eaa8672b0
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-08-28 17:52:26 -0400

    Something is going on with Jest mocks

--- services/metadata/src/index.js
+++ services/metadata/src/index.js
@@ -1,42 +1,63 @@
 const express = require("express");
 const mongodb = require("mongodb");
-const bodyParser = require("body-parser");
 
 function connectDb(dbhost, dbname) {
-    return mongodb.MongoClient.connect(dbhost) 
+    return mongodb.MongoClient.connect(dbhost, { useUnifiedTopology: true }) 
         .then(client => {
-            return client.db(dbname);
+            const db = client.db(dbname);
+            return {                
+                db: db,             
+                close: () => {      
+                    return client.close();
+                },
+            };
         });
 }
 
-function setupHandlers(app, db) {
+function setupHandlers(microservice) {
 
-    const videosCollection = db.collection("videos");
+    const videosCollection = microservice.db.collection("videos");
 
-    app.get("/videos", (req, res) => {
+    microservice.app.get("/videos", (req, res) => {
         videosCollection.find()
-            .toArray() // TODO Pagination
+            .toArray()
             .then(videos => {
-                res.json({ videos });
+                res.json({ 
+                    videos: videos
+                });
             })
             .catch(err => {
-                console.error("Failed to get videos collection.");
-                console.error(err);
+                console.error("Failed to get videos collection from database!");
+                console.error(err && err.stack || err);
                 res.sendStatus(500);
             });
     });
 }
 
-function startHttpServer(db,) {
+function startHttpServer(dbConn) {
     return new Promise(resolve => {
         const app = express();
-        app.use(bodyParser.json());
-        setupHandlers(app, db);
+        console.log(express());
+        const microservice = {
+            app: app,
+            db: dbConn.db,
+        }
+        setupHandlers(microservice);
 
         const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
-        app.listen(port, () => {
-            resolve();
-        });
+        const server = app.listen(port, () => { console.log('started service on port ' + port) });
+
+        microservice.close = () => {
+            return new Promise(resolve => {
+                server.close(() => {
+                    resolve();
+                });
+            })
+            .then(() => {
+                return dbConn.close();
+            });
+        };
+        resolve(microservice);
     });
 }
 
